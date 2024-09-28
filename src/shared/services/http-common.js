4a1d736745e13278dbc8df97c397ae1c23a1b549
Rodrigo Chirinos <u202217804@upc.edu.pe>
2024-09-28 00:08:15 -0500

    Update http-common.js

--- src/shared/services/http-common.js
+++ src/shared/services/http-common.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 const http = axios.create({
-    baseURL: API_BASE_URL
+    baseURL: 'https://my-json-server.typicode.com/zrodrigochirinos/json-server-interaction'
 });
 
 http.defaults.headers.common["Content-Type"] = "application/json";
