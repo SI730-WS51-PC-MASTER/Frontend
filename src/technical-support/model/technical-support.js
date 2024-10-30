57937382b0846924b527eb78648b11099532c65a
Aranda Vallejos, Oscar Gabriel <134239824+OscarGAV@users.noreply.github.com>
2024-10-30 00:12:34 -0500

    feat: Create technical-support.js

---
+++ src/technical-support/model/technical-support.js
@@ -0,0 +1,9 @@
+// technical-support.js
+export class TechnicalSupport {
+    constructor(data) {
+        this.id = data.id;
+        this.supportType = data.supportType;
+        this.technicianId = data.technicianId;
+        // Add other properties if needed
+    }
+}
